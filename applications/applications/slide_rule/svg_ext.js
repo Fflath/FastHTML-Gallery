1adcb4e65bc7326a13915abe0d056e4e94263561
fflath <felix@fflath.email>
2024-09-05 06:23:49 -0400

    extension improvements

--- applications/applications/slide_rule/svg_ext.js
+++ applications/applications/slide_rule/svg_ext.js
@@ -1,17 +1,5 @@
 (function svgExt(){
-
-    var api
-
     htmx.defineExtension('svg-ext', {
-        // onEvent : function(name, evt) {
-        //     if (name === 'htmx:oobBeforeSwap') {
-        //         console.log("OOB Before Swap");
-        //         console.log(evt);
-        //         this.handleSwap(evt.detail.swapStyle, evt.detail.target, evt.detail.fragment, evt.detail.settleInfo);
-        //         console.log("OOB After Swap");
-        //     }
-        //     // console.log("Fired event: " + name, evt);
-        // },
 
         handleSwap: function(swapStyle, target, fragment, settleInfo) { 
             function add_children(target, node) {
@@ -19,85 +7,58 @@
                     add_child(target, child);
                 }
             }
-            function convertNS(node) {
-                let newNode = document.createElementNS("http://www.w3.org/2000/svg", node.tagName.toLowerCase());
-                for (let attr of node.attributes) {
-                    newNode.setAttributeNS(null, attr.name, attr.value);
+            function convertNS(fragment) {
+                let newSVGContainer = document.createElementNS("http://www.w3.org/2000/svg", fragment.firstElementChild.tagName.toLowerCase());
+                for (let attr of fragment.firstElementChild.attributes) {
+                    newSVGContainer.setAttributeNS(null, attr.name, attr.value);
                 }
-                add_children(newNode, node);
-                return newNode;
+                add_children(newSVGContainer, fragment.firstElementChild);
+                return newSVGContainer;
             }
             
-            function add_child(target, child) {
-                let newSVG = document.createElementNS("http://www.w3.org/2000/svg", child.tagName.toLowerCase());
-                for (let attr of child.attributes) {
-                    newSVG.setAttributeNS(null, attr.name, attr.value);
-                }
-                target.appendChild(newSVG);
-                
-                // Copy text content if present
-                if (child.childNodes.length > 0) {
-                    for (let node of child.childNodes) {
-                        if (node.nodeType === Node.TEXT_NODE) {
-                            newSVG.appendChild(document.createTextNode(node.textContent));
-                        } else if (node.nodeType === Node.ELEMENT_NODE) {
-                            add_child(newSVG, node);
-            }}}};
             // Check if fragment is not a DocumentFragment and wrap it if necessary
             if (!(fragment instanceof DocumentFragment)) {
                 let tempFragment = document.createDocumentFragment();
                 tempFragment.appendChild(fragment);
                 fragment = tempFragment;
             }
+            let newSVGContainer = convertNS(fragment);
+
             switch(swapStyle){
             case "innerSVG":
                 (function(){
-                    let newSVGContainer = document.createElementNS("http://www.w3.org/2000/svg", fragment.firstElementChild.tagName.toLowerCase());
-                    // Add attributes of the fragment's first child to newSVGContainer
-                    for (let attr of fragment.firstElementChild.attributes) {
-                        newSVGContainer.setAttributeNS(null, attr.name, attr.value);
-                    }
-                    for (let child of fragment.children) {
-                        add_child(newSVGContainer, child);
-                    }
                     while (target.firstChild) {
                         target.removeChild(target.firstChild);
                     }
-                    target.appendChild(newSVGContainer);
-                    htmx.process(document.body)})();
+                    target.appendChild(newSVGContainer);})();
                 break;
             case "outerSVG":
                 (function(){
-                    let newSVG = document.createElementNS("http://www.w3.org/2000/svg",fragment.firstElementChild.tagName.toLowerCase())
-                    for (let attr of fragment.firstElementChild.attributes) {
-                        newSVG.setAttributeNS(null, attr.name, attr.value);
-                    }
-                    target=newSVG;
-                    htmx.process(document.body)
+                    target.parentNode.replaceChild(newSVGContainer, target);
                     target.remove()})();
                 break;
             case "update":
                 for (let attr of fragment.firstElementChild.attributes) {
                     target.setAttributeNS(null, attr.name, attr.value);
                 }
-                htmx.process(document.body)
                 break;
-            case "beforeend":
-                (function(){
-                // let newSVGContainer = document.createElementNS("http://www.w3.org/2000/svg", fragment.firstElementChild.tagName.toLowerCase());
-                // for (let attr of fragment.firstElementChild.attributes) {
-                //     newSVGContainer.setAttributeNS(null, attr.name, attr.value);
-                // }
-
-                for (let child of fragment.children) {
-                    add_child(newSVGContainer, child);
-                }
-                
-                target.appendChild(newSVGContainer);
-                htmx.process(document.body)})();
+            case "beforebeginSVG":
+                (function(){target.parentNode.insertBefore(newSVGContainer, target)})();
+                break;
+            case "afterbeginSVG":
+                (function(){target.insertBefore(newSVGContainer, target.firstChild)})();
+                break;
+            case "beforeendSVG":
+                (function(){target.appendChild(newSVGContainer)})();
+                break;
+            case "afterendSVG":
+                (function(){target.parentNode.append(newSVGContainer)})();
+                break;
+            case "deleteSVG":
+                (function(){target.remove()})();
                 break;
             }
-            // api.oobSwap(swapStyle,target,settleInfo)
+            htmx.process(document.body)
             return true}})})();
 
 // htmx.logAll();
